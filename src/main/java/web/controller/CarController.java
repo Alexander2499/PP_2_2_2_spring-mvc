2c6512ff62ace7a443df202f458d97f50fc5be02
Саня <alecs.korotkow@mail.ru>
2024-02-13 18:36:28 +0300

    I create Car

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -3,6 +3,7 @@ package web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import web.model.Car;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,12 +11,40 @@ import java.util.List;
 @Controller
 public class CarController {
     @GetMapping(value = "/cars")
-    public String printWelcome(ModelMap model) {
-        List<String> messages = new ArrayList<>();
-        messages.add("PIZDA");
-        messages.add("I'm Spring MVC application");
-        messages.add("5.2.0 version by sep'19 ");
-        model.addAttribute("messages", messages);
-        return "index";
+    public List<Car> printCars() {
+        List<Car> cars = new ArrayList<>();
+        Car car1 = new Car();
+        car1.setModel("Mazda 6");
+        car1.setSeries(3);
+        car1.setYearOfRelease(2014);
+
+        Car car2 = new Car();
+        car2.setModel("Cadillac Eldorado");
+        car2.setSeries(7);
+        car2.setYearOfRelease(1977);
+
+        Car car3 = new Car();
+        car3.setModel("Cadillac Escalade");
+        car3.setSeries(4);
+        car3.setYearOfRelease(2015);
+
+        Car car4 = new Car();
+        car4.setModel("Cadillac Escalade");
+        car4.setSeries(3);
+        car4.setYearOfRelease(2010);
+
+        Car car5 = new Car();
+        car5.setModel("Land Rover Discovery");
+        car5.setSeries(4);
+        car5.setYearOfRelease(2013);
+
+        cars.add(car1);
+        cars.add(car2);
+        cars.add(car3);
+        cars.add(car4);
+        cars.add(car5);
+
+        return null;
+//        return "index";
     }
 }
